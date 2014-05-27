25234cf3e8968942f010c5d9e20733d50da9a5a0
victor_zacaron <victor_zacaron@10.1.48.234>
2014-05-27 13:35:03 -0300

    Ola Marilene, Hoje a Noite, Tainha, Vinho e Muito Çékso

---
+++ src/teste48porem24/Teste48Porem24.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste48porem24;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author victor_zacaron
+ */
+public class Teste48Porem24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null,"OLA Seus BOROCO");
+    }
+}
